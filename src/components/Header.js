5009036760cfc31f831c62a87b45e16a0935de47
hknsagar <hknsagar@gmail.com>
2022-02-27 02:57:37 +0530

    Show items in cart dropdown

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
 import React from "react";
-import { Container, Navbar, Dropdown, Nav, Badge } from "react-bootstrap";
+import { Container, Navbar, Dropdown, Nav, Badge, Button, Image, Row, Col } from "react-bootstrap";
 import { BsBagCheck, BsCartPlus } from "react-icons/bs";
 import { Link } from "react-router-dom";
 import { CartState } from "../context/CartContext";
+import { FaRupeeSign } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
 
 const Header = () => {
   const {
@@ -26,8 +28,45 @@ const Header = () => {
                 <Badge bg="success">{cart.length}</Badge>
               </Dropdown.Toggle>
 
-              <Dropdown.Menu>
-                <Dropdown.Item disabled>Empty</Dropdown.Item>
+              <Dropdown.Menu style={{ minWidth: "400px" }}>
+                {cart.length === 0 ? (
+                  <Dropdown.Item disabled>Empty</Dropdown.Item>
+                ) : (
+                  <>
+                    {cart.map((item) => (
+                      <>
+                        <Dropdown.Item>
+                          <Row>
+                            <Col md={2} className="pe-0">
+                              <Image
+                                src={item.image}
+                                thumbnail={true}
+                                fluid={true}
+                                style={{ width: "60px" }}
+                              />
+                            </Col>
+                            <Col md={8} className="">
+                              <div style={{ fontSize: "14px" }}>{item.name}</div>
+                              <div style={{ fontSize: "14px" }}>
+                                <strong>
+                                  <FaRupeeSign size="12px" />
+                                  {item.price}
+                                </strong>
+                              </div>
+                            </Col>
+                            <Col md={2} style={{ textAlign: "center" }}>
+                              <MdDelete size="25px" />
+                            </Col>
+                          </Row>
+                        </Dropdown.Item>
+                        <Dropdown.Divider />
+                      </>
+                    ))}
+                    <div className="d-grid gap-2 mx-2">
+                      <Button variant="primary">Go to Cart</Button>
+                    </div>
+                  </>
+                )}
               </Dropdown.Menu>
             </Dropdown>
           </Nav>
